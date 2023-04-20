884322dd63404f9b8b3981d855c1a75379cdf15c
ewinp2 <ewinpalencia123@gmail.com>
2023-04-20 17:16:52 -0400

    primer commit original

---
+++ src/componentes/Navbar/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Navbar = ()=>{
+    return(
+        <Titulo>
+         <Logo>    
+        </Logo>   
+        <NavWrapper></NavWrapper>
+        </Titulo>
+    )
+}
+
+const Titulo = styled.div`
+   width: 100%;
+   height: 100px;
+   display: flex;
+   background-color: lightgray;
+`
+
+const Logo = styled.div`
+   width: 20%;
+   background-color: blue;
+`
+
+const NavWrapper = styled.div`
+   width: 80%;
+   background-color: red;
+`
---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../hojas-de-estilo/Testimonio.css";
+
+function Testimonio(props) {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require(`../componentes/imagenes/testimonio-${props.imagen}.png`)}
+        alt="Foto de shawn"
+      />
+      .
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          {props.nombre} en {props.pais}
+        </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en {props.empresa}
+        </p>
+        <p className="texto-testimonio">{props.testimonio}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
